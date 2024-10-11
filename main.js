ed482e5f9f891928db0b3cba5edb396cf92be0d6
Vinicius Jesus da Silva <vinicius.silva2504@gmail.com>
2024-10-11 17:37:40 +0100

    revisão de códigos de máscara
    revisão nos códigos de aplicação de máscara nos campos telefone, cpf e cep.

--- main.js
+++ main.js
@@ -1,21 +1,13 @@
 $(document).ready(function () {
-    // Inicializa o carrossel
     $('#carousel-imagens').slick({
         autoplay: true,
     });
-
-    // Máscaras para os campos de telefone, CPF e CEP
     $('#telefone').mask('(00) 00000-0000');
     $('#cpf').mask('000.000.000-00');
     $('#cep').mask('00000-000');
-
-    // Validação do formulário
-    $('form').validate();
-
-    // Função para resetar o formulário após o envio
     $('#form').on('submit', function (e) {
-        e.preventDefault(); // Impede o comportamento padrão de envio do formulário
+        e.preventDefault(); 
         alert("Formulário enviado com sucesso!");
-        this.reset(); // Reseta o formulário
+        this.reset();
     });
 });
