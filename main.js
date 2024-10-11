9122e9dd704bf9624718ecd59d06728fdad20109
viniciussilva2504 <vinicius.silva2504@gmail.com>
2024-10-11 17:32:20 +0100

    revisão segundo orientação do tutor: Campo telefone recebeu aplicação da máscara correta, assim como campos cpf e cep

--- main.js
+++ main.js
@@ -2,13 +2,9 @@ $(document).ready(function () {
     $('#carousel-imagens').slick({
         autoplay: true,
     });
-
     $('#telefone').mask('(00) 00000-0000');
-    
     $('#cpf').mask('000.000.000-00');
-    
     $('#cep').mask('00000-000');
-
     $('#form').on('submit', function (e) {
         e.preventDefault(); 
         alert("Formulário enviado com sucesso!");
