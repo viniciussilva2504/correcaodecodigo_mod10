c9bbd48d84d2233fa73412dc68a67ecf81d189d2
Vinicius Jesus da Silva <vinicius.silva2504@gmail.com>
2024-10-14 16:42:42 +0100

    revisão de campos e prioridade de links
    Organizar a ordem de importação dos nossos scripts.
    Verificar que as funções de máscara e validação estão corretas
    E por fim garantir que o nosso carrossel esteja funcional

--- main.js
+++ main.js
@@ -2,12 +2,47 @@ $(document).ready(function () {
     $('#carousel-imagens').slick({
         autoplay: true,
     });
+
     $('#telefone').mask('(00) 00000-0000');
     $('#cpf').mask('000.000.000-00');
     $('#cep').mask('00000-000');
-    $('#form').on('submit', function (e) {
-        e.preventDefault(); 
-        alert("Formulário enviado com sucesso!");
-        this.reset();
+
+    $('#form').validate({
+        rules: {
+            nome: {
+                required: true,
+                minlength: 5,
+                nomeCompleto: true 
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true,
+                minlength: 14
+            },
+            cpf: {
+                required: true,
+                minlength: 14
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true,
+                minlength: 9
+            }
+        },
+        messages: {
+            nome: {
+                required: "Por favor, insira seu nome completo.",
+                minlength: "Seu nome completo deve ter pelo menos 5 caracteres.",
+                nomeCompleto: "Por favor, insira nome e sobrenome."
+            }
+        }
     });
+    $.validator.addMethod("nomeCompleto", function(value, element) {
+        return /\s/.test(value.trim()); 
+    }, "Por favor, insira seu nome completo.");
 });
